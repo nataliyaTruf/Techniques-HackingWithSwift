399989c61504815f4b997c118d36ae1d556fb6e7
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-11-15 18:03:52 +0100

    chore: create AdvancedCanvas project

---
+++ AdvancedCanvas/AdvancedCanvas/AdvancedCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCanvasApp.swift
+//  AdvancedCanvas
+//
+//  Created by Natasha Trufanova on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
