885d3d465d1d371c1f79ade991e066601493c5b6
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-11-16 12:46:43 +0100

    Draw shapes

--- Built-in shapes/Built-in shapes/ContentView.swift
+++ Built-in shapes/Built-in shapes/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("Capsule Clip")
+                .padding()
+                .background(.yellow)
+                .clipShape(.capsule)
+            
+            Text("Rounded Rectangle")
+                .padding()
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 16))
+            
+            Text("Uneven Rounded Rectangle")
+                .padding()
+                .background(.green)
+                .clipShape(.rect(topLeadingRadius: 20, bottomTrailingRadius: 20))
+            
         }
-        .padding()
+        .padding(.top, 30)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        
+        ZStack {
+            Rectangle()
+                .fill(.gray)
+                .frame(width: 200, height: 200)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            UnevenRoundedRectangle(cornerRadii: .init(topLeading: 50, topTrailing: 20))
+                .frame(width: 100, height: 150)
+            
+            Capsule()
+                .fill(.green)
+                .frame(width: 220, height: 120)
+            
+            Ellipse()
+                .fill(.red)
+                .frame(width: 40, height: 70)
+            
+            Circle()
+                .fill(.black)
+                .frame(width: 30)
+            
+        }
     }
 }
 
