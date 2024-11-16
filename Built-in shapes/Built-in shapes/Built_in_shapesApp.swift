30f4b7877dcfad1012eb9fd3063b90bda74a5f0b
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-11-16 12:17:12 +0100

    Chore: create Built-in-shapes project

---
+++ Built-in shapes/Built-in shapes/Built_in_shapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Built_in_shapesApp.swift
+//  Built-in shapes
+//
+//  Created by Natasha Trufanova on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Built_in_shapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
