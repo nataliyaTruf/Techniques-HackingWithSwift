9aecd79099cac92085cd63b2ede868be0dde33db
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-11-19 15:17:44 +0100

    Pre-resolving graphics

--- AdvancedCanvas/AdvancedCanvas/ContentView.swift
+++ AdvancedCanvas/AdvancedCanvas/ContentView.swift
@@ -11,31 +11,20 @@ struct ContentView: View {
     
     var body: some View {
         Canvas { context, size in
-            print("Rendering")
-            let path = Path(roundedRect: CGRect(x: 100, y: 200, width: 200, height: 200), cornerRadius: 25)
+            context.translateBy(x: 150, y: 200)
             
-            if context.environment.colorScheme == .dark {
-                context.fill(path, with: .color(.white))
-            } else {
-                context.fill(path, with: .color(.black))
+            let resolved = context.resolve(Text("Hello world!").font(.title))
+            context.addFilter(.shadow(radius: 3))
+            context.addFilter(.shadow(radius: 3))
+            
+            for i in 1...100 {
+                context.translateBy(x: 1, y: 1)
+                context.opacity = Double(i) / 100
+                context.draw(resolved, at: .zero)
             }
         }
-        
-        
     }
-    
-    func drawCircles(in innerContext: inout GraphicsContext) {
-        let size = 220
-        
-        let circle1 = Path(ellipseIn: CGRect(x: 100, y: 200, width: size, height: size))
-        innerContext.fill(circle1, with: .color(red: 1, green: 0, blue: 0))
-        
-        let circle2 = Path(ellipseIn: CGRect(x: 100, y: 300, width: size, height: size))
-        innerContext.fill(circle2, with: .color(red: 0, green: 1, blue: 0))
-        
-        let circle3 = Path(ellipseIn: CGRect(x: 100, y: 400, width: size, height: size))
-        innerContext.fill(circle3, with: .color(red: 0, green: 0, blue: 1))
-    }
+
 }
 
 #Preview {
